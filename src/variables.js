80d69e4ea7fa2c2509f66953300beaf45a2ed6e6
Thiago Fraga <fraga.thiagoribeiro@gmail.com>
2023-02-07 07:55:16 -0300

    feat: Variables Fonts and Colors

---
+++ src/variables.js
@@ -0,0 +1,13 @@
+export const Colors = {
+  cinza: {
+    1: '#EEEEEE',
+    2: '#D9D9D9',
+  },
+  azulEsverdeado: '#23A4A6',
+  azul: '#203699',
+  azulEscuro: '#123645',
+};
+
+export const Fonts = {
+  montserrat: 'Montserrat, sans-serif;',
+};
