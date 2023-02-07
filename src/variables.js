ef6ca59707a54eba471f54a3451b3e9cc5739783
Thiago Fraga <fraga.thiagoribeiro@gmail.com>
2023-02-07 08:01:50 -0300

    fix: variables names

--- src/variables.js
+++ src/variables.js
@@ -1,11 +1,11 @@
 export const Colors = {
-  cinza: {
+  gray: {
     1: '#EEEEEE',
     2: '#D9D9D9',
   },
-  azulEsverdeado: '#23A4A6',
-  azul: '#203699',
-  azulEscuro: '#123645',
+  greenishBlue: '#23A4A6',
+  blue: '#203699',
+  darkBlue: '#123645',
 };
 
 export const Fonts = {
