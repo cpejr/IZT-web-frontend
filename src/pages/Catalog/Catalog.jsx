65860a4c2fedcc000d17cc3aa101abb9f7ca5e1f
Thiago Fraga <fraga.thiagoribeiro@gmail.com>
2023-02-09 10:44:13 -0300

    feat: product catalog separated by category

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,12 +1,155 @@
 import React from 'react';
-import { Container, Introducao, Titulo } from './Styles';
+import {
+  Container,
+  Introduction,
+  Title,
+  Description,
+  ButtonRow,
+  Button,
+  ProductCategory,
+  CategoryName,
+  Divider,
+  ProductRow,
+  Product,
+  ProductImage,
+  ProductName,
+} from './Styles';
+
+const products = [
+  {
+    Category: 'Tipo 1',
+    Bocals: [
+      {
+        Name: 'Product 1',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+      {
+        Name: 'Product 2',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+      {
+        Name: 'Product 3',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+    ],
+  },
+  {
+    Category: 'Tipo 2',
+    Bocals: [
+      {
+        Name: 'Product 1',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+      {
+        Name: 'Product 2',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+      {
+        Name: 'Product 3',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+    ],
+  },
+  {
+    Category: 'Tipo 3',
+    Bocals: [
+      {
+        Name: 'Product 1',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+      {
+        Name: 'Product 2',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+      {
+        Name: 'Product 3',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+    ],
+  },
+  {
+    Category: 'Tipo 4',
+    Bocals: [
+      {
+        Name: 'Product 1',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+      {
+        Name: 'Product 2',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+      {
+        Name: 'Product 3',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+    ],
+  },
+  {
+    Category: 'Tipo 5',
+    Bocals: [
+      {
+        Name: 'Product 1',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+      {
+        Name: 'Product 2',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+      {
+        Name: 'Product 3',
+        image:
+          'https://www.decorlux.com.br/site2019/wp-content/uploads/2020/07/ddd.jpg',
+      },
+    ],
+  },
+];
 
 function Catalog() {
   return (
     <Container>
-      <Introducao>
-        <Titulo>Tudo o que você precisa está aqui!</Titulo>
-      </Introducao>
+      <Introduction>
+        <Title>Tudo o que você precisa está aqui!</Title>
+        <Description>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          sed odio eu enim gravida varius quis non orci. Curabitur sed placerat
+          sem, eu faucibus diam. Fusce ut nulla sed sapien.
+        </Description>
+      </Introduction>
+      <ButtonRow>
+        {products.map((product) => (
+          <a key={product.Category} href={`#${product.Category}`}>
+            <Button>{product.Category}</Button>
+          </a>
+        ))}
+      </ButtonRow>
+      {products.map((product) => (
+        <ProductCategory key={product.Category}>
+          <Divider />
+          <CategoryName id={product.Category}>{product.Category}</CategoryName>
+          <ProductRow>
+            {product.Bocals.map((bocal) => (
+              <Product key={bocal.Name}>
+                <ProductImage src={bocal.image} />
+                <ProductName>{bocal.Name}</ProductName>
+              </Product>
+            ))}
+          </ProductRow>
+        </ProductCategory>
+      ))}
     </Container>
   );
 }
--- src/pages/Catalog/Styles.jsx
+++ src/pages/Catalog/Styles.jsx
@@ -5,21 +5,111 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   padding: 50px 10%;
-
+  gap: 50px;
   font-family: ${Fonts.montserrat};
   background-color: white;
 `;
 
-export const Introducao = styled.div`
+export const Introduction = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 20px;
 `;
 
-export const Titulo = styled.h1`
+export const Title = styled.h1`
   font-size: 26px;
   font-weight: 700;
   color: ${Colors.blue};
 `;
 
-export const Descricao = styled.p``;
+export const Description = styled.p`
+  font-size: 18px;
+  font-weight: 500;
+  text-align: center;
+  width: 70%;
+`;
+
+export const ButtonRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  gap: 2%;
+  min-width: 100%;
+`;
+
+export const Button = styled.button`
+  width: auto;
+  padding: 5px 50px;
+  font-family: ${Fonts.montserrat};
+  font-weight: 500;
+  font-size: 20px;
+  background-color: ${Colors.darkBlue};
+  border-radius: 10px;
+  border: none;
+  color: white;
+`;
+
+export const ProductCategory = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Divider = styled.div`
+  height: 2px;
+  width: 300px;
+  border: 1px solid ${Colors.blue};
+  background-color: ${Colors.blue};
+  margin-bottom: 50px;
+`;
+
+export const CategoryName = styled.h2`
+  font-family: ${Fonts.montserrat};
+  font-weight: 600;
+  font-size: 25px;
+  color: ${Colors.blue};
+  margin-bottom: 30px;
+`;
+
+export const ProductRow = styled.div`
+  width: 100%;
+  display: grid;
+  gap: 100px;
+  grid-template-columns: 250px 250px 250px;
+  grid-auto-flow: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Product = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  width: 100%;
+  height: 300px;
+  padding: 5px;
+  border-radius: 20px;
+
+  :hover {
+    border: 2px solid ${Colors.blue};
+  }
+`;
+
+export const ProductImage = styled.img`
+  display: flex;
+  height: auto;
+  width: auto;
+  max-height: 100%;
+  max-width: 100%;
+`;
+
+export const ProductName = styled.h3`
+  font-family: ${Fonts.montserrat};
+  font-weight: 500;
+  font-size: 20px;
+`;
