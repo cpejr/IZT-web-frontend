13afc65bf0af12b323726108b68bc878ee40139f
Alexandre Cenachi <alexandrecenachi@cpejr.com.br>
2023-02-09 13:32:05 -0300

    feat: creating home skeleton

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Container } from "./Styles";
+
+
+function Home (){
+  return(
+    <Container>
+      <div>
+        Carousel
+      </div>
+      <div>
+        Cards
+      </div>
+      <div>
+        Contact inputs
+      </div>
+      <div>
+        About us
+      </div>
+    </Container>
+  )
+}
+
+
+export default Home;
---
+++ src/pages/Home/Styles.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { Colors, Fonts } from "../../variables";
+
+export const Container = styled.div`
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+`
