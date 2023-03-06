b9ba08c2bc595aeef1d5f11f7b3f3f6fa00155b2
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-03-06 09:08:26 -0300

    feat: creating adm route

---
+++ src/pages/AdministratorPage/AdministratorPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container } from './Styles';
+
+function AdministratorPage() {
+  return <Container>Oiee</Container>;
+}
+
+export default AdministratorPage;
---
+++ src/pages/AdministratorPage/Styles.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  width: 100%;
+  height: 100%;
+  padding-top: 10px;
+  max-width: 1440px;
+  align-items: center;
+  justify-content: center;
+`;
