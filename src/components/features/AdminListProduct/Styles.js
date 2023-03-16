84e472db1359ffd7bc2355f68c29b3c7a0892857
Thiago Fraga <fraga.thiagoribeiro@gmail.com>
2023-03-16 08:15:25 -0300

    fix: list product table

--- src/components/features/AdminListProduct/Styles.js
+++ src/components/features/AdminListProduct/Styles.js
@@ -100,8 +100,8 @@ export const SearchProduct = styled.input`
 `;
 
 export const Row = styled.div`
-  display: flex;
-  flex-direction: row;
+  display: grid;
+  grid-template-columns: 1fr 1fr 30px;
   justify-content: space-between;
 `;
 
