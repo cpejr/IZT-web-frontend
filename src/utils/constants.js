6c74c74e5afd315b82720197d042d488eb8bd3cb
valentina <valentinavpsfacul@gmail.com>
2023-03-17 07:29:13 -0300

    feat: constants file

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/prefer-default-export
+export const ERROR_CODES = {
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INTERNAL_SERVER: 500,
+};
