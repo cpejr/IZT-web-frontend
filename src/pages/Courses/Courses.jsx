7fb31d310da912e0da1f45ee157a0618ca4e7188
Alexandre Cenachi <alexandrecenachi@cpejr.com.br>
2023-04-07 10:47:52 -0300

    style: responsive fonts

--- src/pages/Courses/Courses.jsx
+++ src/pages/Courses/Courses.jsx
@@ -144,40 +144,6 @@ export default function Courses() {
             </div>
 
             <SubtitleScroll>Capítulo 3</SubtitleScroll>
-            <GreyLine />
-            <div>
-              <TopicDiv>
-                <div>
-                  <ScrollIcon src={FinishedVideo} alt="finishedVideo" />
-                  <Topics>Introdução</Topics>
-                </div>
-                <VideoTime>15:23</VideoTime>
-              </TopicDiv>
-
-              <TopicDiv>
-                <div>
-                  <ScrollIcon src={FinishedVideo} alt="finishedVideo" />
-                  <Topics>Vídeo 1</Topics>
-                </div>
-                <VideoTime>09:10</VideoTime>
-              </TopicDiv>
-
-              <TopicDiv>
-                <div>
-                  <ScrollIcon src={UnfinishedVideo} alt="unfinishedVideo" />
-                  <Topics>Vídeo 2</Topics>
-                </div>
-                <VideoTime>05:43</VideoTime>
-              </TopicDiv>
-
-              <TopicDiv>
-                <div>
-                  <ScrollIcon src={UnfinishedVideo} alt="unfinishedVideo" />
-                  <Topics>Vídeo 3</Topics>
-                </div>
-                <VideoTime>03:25</VideoTime>
-              </TopicDiv>
-            </div>
           </CoursesScroll>
 
           <VideoSectionDiv>
--- src/pages/Courses/Styles.js
+++ src/pages/Courses/Styles.js
@@ -7,7 +7,8 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
 
-  padding-bottom: 62px;
+  margin-bottom: 5%;
+  padding: 3%;
   width: 100%;
   height: auto;
 `;
@@ -28,16 +29,15 @@ export const IntroductionDiv = styled.div`
 
 export const Title = styled.p`
   font-weight: 700;
-  font-size: 30px;
-  margin-top: 36px;
-  margin-bottom: 17px;
+  font-size: 1.87rem;
+  margin-bottom: 1rem;
 
   color: ${(props) => props.theme.colors.blue};
 `;
 
 export const Text = styled.p`
   font-weight: 400;
-  font-size: 18px;
+  font-size: 1.1rem;
 
   padding-bottom: 20px;
   width: 100%;
@@ -49,6 +49,7 @@ export const MainSection = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  gap: 2em;
 
   p {
     margin-top: 0px;
@@ -57,31 +58,31 @@ export const MainSection = styled.div`
   div {
     gap: 15px;
   }
+  @media (max-width: 1000px) {
+    flex-direction: column;
+  }
 `;
 
 export const CoursesScroll = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  gap: 15px;
 
   max-width: 250px;
   width: 100%;
   height: 100%;
-  margin-right: 50px;
-  overflow-y: scroll;
 `;
 
 export const SubtitleScroll = styled.p`
   font-weight: 600;
-  font-size: 25px;
+  font-size: 1.6rem;
   line-height: 30px;
 
   color: ${(props) => props.theme.colors.blue};
 `;
 
 export const GreyLine = styled.div`
-  width: 250px;
+  width: 100%;
   height: 2px;
 
   background: #d9d9d9;
@@ -101,13 +102,13 @@ export const TopicDiv = styled.div`
 `;
 
 export const ScrollIcon = styled.img`
-  width: 20px;
-  height: 20px;
+  width: 1.2rem;
+  height: 1.2rem;
 `;
 
 export const Topics = styled.p`
   font-weight: 500;
-  font-size: 20px;
+  font-size: 1.2rem;
   line-height: 24px;
 
   color: #202020;
@@ -115,13 +116,14 @@ export const Topics = styled.p`
 
 export const VideoTime = styled.p`
   font-weight: 400;
-  font-size: 16px;
+  font-size: 1rem;
 
   color: #a5a5a5;
 `;
 
 export const VideoSectionDiv = styled.div`
-  width: 982px;
+  width: 100%;
+  max-width: 982px;
   height: 735.25px;
 `;
 
@@ -130,7 +132,8 @@ export const Picture = styled.img`
   justify-content: center;
   align-items: center;
 
-  width: 980px;
+  width: 100%;
+  max-width: 980px;
   height: auto;
 
   margin-bottom: 25px;
