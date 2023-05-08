35dfd193e16859ef975317f980676518f9077bda
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-05-08 15:46:04 -0300

    fix: responsive mobile

--- src/pages/ConfirmedEmail/Styles.js
+++ src/pages/ConfirmedEmail/Styles.js
@@ -1,15 +1,29 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  margin-top: 100px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   width: 100%;
-  height: 50vh;
+  height: 60vh;
+
+  @media (max-width: 700px) {
+    margin-top: 0px;
+    height: 90vh;
+  }
 `;
 
 export const Title = styled.h1`
-  padding-top: 25px;
+  width: 80%;
+  text-align: center;
+  padding-top: 50px;
   font-family: ${(props) => props.theme.fonts.montserrat};
+
+  @media (max-width: 700px) {
+    font-size: 18px;
+  }
+  @media (max-width: 400px) {
+    font-size: 15px;
+  }
 `;
