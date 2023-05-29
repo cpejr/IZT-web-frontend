2434ef9801000791740af75dcb6cea6acdbd5a83
joaopedropiraja <jplp100@hotmail.com>
2023-05-28 23:39:25 -0300

    fix: delete component scroll_to_top and transfer its functionalities to the app_layout

--- src/layouts/AppLayout/AppLayout.jsx
+++ src/layouts/AppLayout/AppLayout.jsx
@@ -1,17 +1,21 @@
 import { useEffect, useState } from 'react';
 
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import 'react-toastify/dist/ReactToastify.css';
 import { AddToast } from '../../components/common';
 import { Header, Footer, SystemLoading } from '../../components/features';
 import { useRefreshToken } from '../../hooks/query/sessions';
-import ScrollToTop from '../../utils/ScrollToTop';
 
 export default function AppLayout() {
-  const { isInitialLoading } = useRefreshToken();
   const [isLoadingScreen, setIsLoadingScreen] = useState(true);
+  const { isInitialLoading } = useRefreshToken();
+  const { pathname } = useLocation();
   const timeToShowLoading = 2000; // milliseconds
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll page to top when it is opened
+  }, [pathname]);
 
   useEffect(() => {
     const loadingTimer = setTimeout(
@@ -26,7 +30,6 @@ export default function AppLayout() {
     <SystemLoading />
   ) : (
     <>
-      <ScrollToTop />
       <Header />
       <Outlet />
       <AddToast />
