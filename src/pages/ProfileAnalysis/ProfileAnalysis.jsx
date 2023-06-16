2af67cae9e9656453a190ddb6921006857146071
Ricardo Li <ricardoli@cpejr.com.br>
2023-06-16 16:36:06 -0300

    fix: profile analysis ajusments

--- src/pages/ProfileAnalysis/ProfileAnalysis.jsx
+++ src/pages/ProfileAnalysis/ProfileAnalysis.jsx
@@ -1,4 +1,5 @@
 import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
+import {HiOutlinePencil} from 'react-icons/hi';
 
 import {
   Container,
@@ -9,11 +10,35 @@ import {
   ProductData,
   RA,
   Text,
+  Hw,
+  Hd,
+  Analysis,
+  Container2,
+  OutputData,
+  Adr,
+  InclDresser,
+  RullerDegree,
+  Nr1,
+  Nr2,
+  Vp,
+  Vfa,
+  Text2,
+  Qp,
+  Tc,
+  U,
+  Qw,
+  Hef,
+  Y,
+  Yra,
+  Yrc,
+  Containerleft,
+  ContainerRight,
 } from './Styles';
 
 export default function ProfileAnalysis() {
   return (
     <Container>
+      <Containerleft>
       <DataInput>
         <h1>Entrada de dados</h1>
         <Datas>
@@ -35,6 +60,73 @@ export default function ProfileAnalysis() {
           </RA>
         </Datas>
       </DataInput>
+      </Containerleft>
+      <Container>
+        <Analysis>
+        <h1>Analise #1 <HiOutlinePencil size={20} color="white"/></h1> 
+        <Container2></Container2>
+        <ContainerRight>
+        <OutputData>
+          <h1>Dados de saída</h1>
+          <Hw>
+           <Text2> Altura entre centros (hw): ___ mm</Text2>
+          </Hw>
+          <Hd>
+          <Text2>Altura do dessador (hd): ___ mm</Text2>
+          </Hd>
+          <Adr>
+             <Text2>Inclinação do RA (adr): ___ °</Text2>
+          </Adr>
+          <InclDresser>
+          <Text2>Inclinação do dressador: ___ °</Text2>
+            </InclDresser>
+          <RullerDegree>
+          <Text2>Ângulo da régua (β): ___ °</Text2>
+          </RullerDegree>
+          <Nr1>
+          <Text2>Rotação do rebolo de arraste (nr): ___ rpm</Text2>
+          </Nr1>
+          <Nr2>
+          <Text2>Rotação do rebolo de arraste (nr): ___ rps</Text2>
+          </Nr2>
+          <Vp>
+          <Text2>Velocidade periférica da peça (vp): ___ m/s</Text2>
+          </Vp>
+          <Vfa>
+          <Text2>Velocidade de passagem da peça (vfa): ___ m/min</Text2>
+          </Vfa>
+        </OutputData>
+        <OutputData>
+        <Qp>
+           <Text2> Quantidade de peça (Qp): ___ </Text2>
+          </Qp>
+        <Tc>
+           <Text2> Tempo de ciclo (tc): ___ min/pc</Text2>
+          </Tc>
+        <U>
+           <Text2> Nr. revoluções da peça (U): ___ </Text2>
+          </U>
+        <Qw>
+           <Text2> Taxa de remoção (Qw’): ___ mm3/mm.s</Text2>
+          </Qw>
+        <Hef>
+           <Text2> Espessura de corte (hef): ___ mm</Text2>
+          </Hef>
+        <Y>
+           <Text2> Ângulo de tangêcia (γ): ___ °</Text2>
+          </Y>
+        <Yra>
+           <Text2> Ângulo de tangência RA (γra): ___ °</Text2>
+          </Yra>
+        <Yrc>
+           <Text2> Ângulo de tangência RC (γrc) ___ °</Text2>
+          </Yrc>
+          
+        </OutputData>
+        </ContainerRight>
+        </Analysis>
+      </Container>
     </Container>
+  
   );
 }
--- src/pages/ProfileAnalysis/Styles.js
+++ src/pages/ProfileAnalysis/Styles.js
@@ -2,12 +2,13 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  align-items: center;
+  //align-items: center;
   flex-direction: row;
   background-color: #1d4f69;
 `;
 export const DataInput = styled.div`
   h1 {
+    margin-top: 5%;
     font-family: 'Montserrat';
     font-style: normal;
     font-weight: 600;
@@ -17,11 +18,116 @@ export const DataInput = styled.div`
     color: #ffffff;
   }
 `;
+export const Containerleft = styled.div`
+  margin-left: 5%;
+  height: 100%;
+  border-right: dashed;
+  border-color: #ffffff;
+  display: flex;
+  //align-items: center;
+  flex-direction: row;
+  background-color: #1d4f69;
+`;
+
+export const ContainerRight = styled.div`
+  margin-left: 10%;
+  //height: 100%;
+  //border-right: dashed;
+  //border-color: #ffffff;
+  display: flex;
+  //align-items: center;
+  flex-direction: row;
+  background-color: #1d4f69;
+`;
+
+export const Container2 = styled.div`
+  margin: 5%;
+  display: flex;
+  align-items: center;
+  background-color: #ffffff;
+  height: 360px;
+  width: 640px;
+`;
+
+export const Analysis = styled.div`
+  h1 {
+    margin-top: 2%;
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 29px;
+    margin-left: 5px;
+
+    color: #ffffff;
+  }
+`;
+
+export const OutputData = styled.div`
+  margin-left: 5%;
+  margin-bottom: 5%;
+  flex-direction: row;
+  border-right: dashed;
+  border-color: #ffffff;
+  h1 {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 15px;
+    line-height: 29px;
+    color: #ffffff;
+  }
+`;
+
+export const OutputData2 = styled.div`
+  margin-bottom: 5%;
+  border-right: dashed;
+  border-color: #ffffff;
+  h1 {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 15px;
+    line-height: 29px;
+    color: #ffffff;
+  }
+`;
+
+export const Hw = styled.div``;
+export const Hd = styled.div``;
+export const Adr = styled.div``;
+export const InclDresser = styled.div``;
+export const RullerDegree = styled.div``;
+export const Nr1 = styled.div``;
+export const Nr2 = styled.div``;
+export const Vp = styled.div``;
+export const Vfa = styled.div``;
+export const Qp = styled.div``;
+export const Tc = styled.div``;
+export const U = styled.div``;
+export const Qw = styled.div``;
+export const Hef = styled.div``;
+export const Y = styled.div``;
+export const Yra = styled.div``;
+export const Yrc = styled.div``;
+
+export const Text2 = styled.a`
+  font-family: 'Montserrat';
+  font-style: normal;
+  font-weight: 300;
+  font-size: 12px;
+  line-height: 15px;
+  margin-left: 3%;
+
+  color: #ffffff;
+`;
+
 export const Datas = styled.div``;
 export const DataAnalysis = styled.div``;
 export const MachineData = styled.div``;
 export const ProductData = styled.div``;
 export const RA = styled.div``;
+
 export const Text = styled.a`
   font-family: 'Montserrat';
   font-style: normal;
