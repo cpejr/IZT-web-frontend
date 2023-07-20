0571eba4b59ca1dd3aa15c99920a775f9638a8d7
Rian-Vieira <rianvieira@cpejr.com.br>
2023-07-20 19:48:39 -0300

    fix: expand bug

--- src/components/features/Report/StabilityAnalysisReport.jsx
+++ src/components/features/Report/StabilityAnalysisReport.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 
-import { CloseOutlined, DownOutlined } from '@ant-design/icons';
+import { DownOutlined, CloseOutlined } from '@ant-design/icons';
 import PropTypes from 'prop-types';
 
 import {
@@ -17,9 +17,7 @@ import {
   DashedBar,
   ModalStyle,
 } from './Styles';
-
 import ModalDeleteStabilityAnalysis from '../ModalDeleteStabilityAnalysis/ModalDeleteStabilityAnalysis';
-import { useGetStabilityAnalysis } from '../../../hooks/query/stabilityAnalysis';
 import useAuthStore from '../../../stores/auth';
 
 export default function StabilityAnalysisReport({
@@ -27,9 +25,11 @@ export default function StabilityAnalysisReport({
   openedReport,
   handleOpened,
 }) {
-  const user = useAuthStore((state) => state.auth?.user);
-  const [isOpened, setIsOpened] = useState(openedReport === data.name);
+  const [isOpened, setIsOpened] = useState(openedReport === data);
 
+  useEffect(() => {
+    setIsOpened(openedReport === data);
+  }, [openedReport, data]);
   const modalCloseButton = <CloseOutlined style={{ color: 'white' }} />;
   const [modalDeleteStabilityAnalysis, setModalDeleteStabilityAnalysis] =
     useState(false);
@@ -40,310 +40,146 @@ export default function StabilityAnalysisReport({
     setModalDeleteStabilityAnalysis(false);
   };
 
-  useEffect(() => {
-    setIsOpened(openedReport === data.name);
-  }, [openedReport, data.name]);
-
-  const { data: stabilityAnalysis } = useGetStabilityAnalysis({});
-  const userStabilityAnalysis = stabilityAnalysis?.filter(
-    (stability) => stability?.user === user?._id
-  );
-  // if (user?.isAdmin) {
-  //   return (
-  //     <div>
-  //       {stabilityAnalysis?.map((stability) => (
-  //         <>
-  //           <ReportName
-  //             isOpened={isOpened}
-  //             onClick={() => handleOpened(data?.name)}
-  //           >
-  //             {stability?.name}
-  //             <DownOutlined />
-  //           </ReportName>
-  //           <Container isOpened={isOpened}>
-  //             <Columns>
-  //               <DataColumn>
-  //                 <Title>Dados da análise</Title>
-  //                 <DataContainer>
-  //                   <DataRow>
-  //                     <Label>Processo retificação:</Label>
-  //                     <Data>{stability?.rectificationProcess}</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Máquina:</Label>
-  //                     <Data>{stability?.machine}</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>N° da máquina:</Label>
-  //                     <Data>{stability?.machineNumber}</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Operação:</Label>
-  //                     <Data>{stability?.operation}</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Departamento:</Label>
-  //                     <Data>{stability?.department}</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Responsável:</Label>
-  //                     <Data>{stability?.responsiblePerson}</Data>
-  //                   </DataRow>
-  //                 </DataContainer>
-  //               </DataColumn>
-  //               <DashedBar />
-  //               <DataColumn>
-  //                 <Title>Dados do produto</Title>
-  //                 <DataContainer>
-  //                   <DataRow>
-  //                     <Label>Produto:</Label>
-  //                     <Data>{stability?.product}</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>N do produto:</Label>
-  //                     <Data>{stability?.productNumber}</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Diâmetro:</Label>
-  //                     <Data>{stability?.diameter} mm</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Compriment total:</Label>
-  //                     <Data>{stability?.totalLength} mm</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Comprimeto eletivo:</Label>
-  //                     <Data>{stability?.electiveLength} mm</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Sobremetal:</Label>
-  //                     <Data>{stability?.allowance} mm</Data>
-  //                   </DataRow>
-  //                 </DataContainer>
-  //               </DataColumn>
-  //               <DashedBar />
-  //               <DataColumn>
-  //                 <Title>Dados da máquina</Title>
-  //                 <DataContainer>
-  //                   <DataRow>
-  //                     <Label>Diâmetro do RC (min):</Label>
-  //                     <Data>{stability?.rcMaxDiameter} mm</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Diâmetro do RC (min):</Label>
-  //                     <Data>{stability?.rcMinDiameter} mm</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Diâmetro do RA:</Label>
-  //                     <Data>{stability?.raDiameter} mm</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Comprimento RC:</Label>
-  //                     <Data>{stability?.rcLength} mm</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Comprimento RA:</Label>
-  //                     <Data>{stability?.raLength} mm</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Comprimento efetivo RC:</Label>
-  //                     <Data>{stability?.rcEffectiveLength} mm</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Rotação do RC:</Label>
-  //                     <Data>{stability?.rcRotation} mm</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Rotação do RA:</Label>
-  //                     <Data>{stability?.raRotation} mm</Data>
-  //                   </DataRow>
-  //                   <DataRow>
-  //                     <Label>Inclinação RW:</Label>
-  //                     <Data>{stability?.rwInclination}</Data>
-  //                   </DataRow>
-  //                 </DataContainer>
-  //               </DataColumn>
-  //             </Columns>
-  //             <ButtonRow>
-  //               <button type="button">Baixar relatório</button>
-  //               <button
-  //                 type="button"
-  //                 onClick={() => openModalDeleteStabilityAnalysis()}
-  //               >
-  //                 Excluir
-  //               </button>
-  //             </ButtonRow>
-  //             <ModalStyle
-  //               open={modalDeleteStabilityAnalysis}
-  //               onCancel={closeModalDeleteStabilityAnalysis}
-  //               footer={null}
-  //               width={500}
-  //               closeIcon={modalCloseButton}
-  //               destroyOnClose
-  //               centered
-  //               bodyStyle={{
-  //                 background: '#123645',
-  //                 color: 'white',
-  //               }}
-  //             >
-  //               <ModalDeleteStabilityAnalysis
-  //                 close={closeModalDeleteStabilityAnalysis}
-  //               />
-  //             </ModalStyle>
-  //           </Container>
-  //         </>
-  //       ))}
-  //     </div>
-  //   );
-  // } else {
   return (
     <div>
-      {userStabilityAnalysis?.map((stability) => (
-        <>
-          <ReportName
-            isOpened={isOpened}
-            onClick={() => handleOpened(data?.name)}
+      <ReportName isOpened={isOpened} onClick={() => handleOpened(data)}>
+        {data.name}
+        <DownOutlined />
+      </ReportName>
+      <Container isOpened={isOpened}>
+        <Columns>
+          <DataColumn>
+            <Title>Dados da análise</Title>
+            <DataContainer>
+              <DataRow>
+                <Label>Processo retificação:</Label>
+                <Data>{data?.rectificationProcess}</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Máquina:</Label>
+                <Data>{data?.machine}</Data>
+              </DataRow>
+              <DataRow>
+                <Label>N° da máquina:</Label>
+                <Data>{data?.machineNumber}</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Operação:</Label>
+                <Data>{data?.operation}</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Departamento:</Label>
+                <Data>{data?.department}</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Responsável:</Label>
+                <Data>{data?.responsiblePerson}</Data>
+              </DataRow>
+            </DataContainer>
+          </DataColumn>
+          <DashedBar />
+          <DataColumn>
+            <Title>Dados do produto</Title>
+            <DataContainer>
+              <DataRow>
+                <Label>Produto:</Label>
+                <Data>{data?.product}</Data>
+              </DataRow>
+              <DataRow>
+                <Label>N do produto:</Label>
+                <Data>{data?.productNumber}</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Diâmetro:</Label>
+                <Data>{data?.diameter} mm</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Compriment total:</Label>
+                <Data>{data?.totalLength} mm</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Comprimeto eletivo:</Label>
+                <Data>{data?.electiveLength} mm</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Sobremetal:</Label>
+                <Data>{data?.allowance} mm</Data>
+              </DataRow>
+            </DataContainer>
+          </DataColumn>
+          <DashedBar />
+          <DataColumn>
+            <Title>Dados da máquina</Title>
+            <DataContainer>
+              <DataRow>
+                <Label>Diâmetro do RC (min):</Label>
+                <Data>{data?.rcMaxDiameter} mm</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Diâmetro do RC (min):</Label>
+                <Data>{data?.rcMinDiameter} mm</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Diâmetro do RA:</Label>
+                <Data>{data?.raDiameter} mm</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Comprimento RC:</Label>
+                <Data>{data?.rcLength} mm</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Comprimento RA:</Label>
+                <Data>{data?.raLength} mm</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Comprimento efetivo RC:</Label>
+                <Data>{data?.rcEffectiveLength} mm</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Rotação do RC:</Label>
+                <Data>{data?.rcRotation} mm</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Rotação do RA:</Label>
+                <Data>{data?.raRotation} mm</Data>
+              </DataRow>
+              <DataRow>
+                <Label>Inclinação RW:</Label>
+                <Data>{data?.rwInclination}</Data>
+              </DataRow>
+            </DataContainer>
+          </DataColumn>
+        </Columns>
+        <ButtonRow>
+          <button type="button">Baixar relatório</button>
+          <button
+            type="button"
+            onClick={() => openModalDeleteStabilityAnalysis()}
           >
-            {stability?.name}
-            <DownOutlined />
-          </ReportName>
-          <Container isOpened={isOpened}>
-            <Columns>
-              <DataColumn>
-                <Title>Dados da análise</Title>
-                <DataContainer>
-                  <DataRow>
-                    <Label>Processo retificação:</Label>
-                    <Data>{stability?.rectificationProcess}</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Máquina:</Label>
-                    <Data>{stability?.machine}</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>N° da máquina:</Label>
-                    <Data>{stability?.machineNumber}</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Operação:</Label>
-                    <Data>{stability?.operation}</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Departamento:</Label>
-                    <Data>{stability?.department}</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Responsável:</Label>
-                    <Data>{stability?.responsiblePerson}</Data>
-                  </DataRow>
-                </DataContainer>
-              </DataColumn>
-              <DashedBar />
-              <DataColumn>
-                <Title>Dados do produto</Title>
-                <DataContainer>
-                  <DataRow>
-                    <Label>Produto:</Label>
-                    <Data>{stability?.product}</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>N do produto:</Label>
-                    <Data>{stability?.productNumber}</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Diâmetro:</Label>
-                    <Data>{stability?.diameter} mm</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Compriment total:</Label>
-                    <Data>{stability?.totalLength} mm</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Comprimeto eletivo:</Label>
-                    <Data>{stability?.electiveLength} mm</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Sobremetal:</Label>
-                    <Data>{stability?.allowance} mm</Data>
-                  </DataRow>
-                </DataContainer>
-              </DataColumn>
-              <DashedBar />
-              <DataColumn>
-                <Title>Dados da máquina</Title>
-                <DataContainer>
-                  <DataRow>
-                    <Label>Diâmetro do RC (min):</Label>
-                    <Data>{stability?.rcMaxDiameter} mm</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Diâmetro do RC (min):</Label>
-                    <Data>{stability?.rcMinDiameter} mm</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Diâmetro do RA:</Label>
-                    <Data>{stability?.raDiameter} mm</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Comprimento RC:</Label>
-                    <Data>{stability?.rcLength} mm</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Comprimento RA:</Label>
-                    <Data>{stability?.raLength} mm</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Comprimento efetivo RC:</Label>
-                    <Data>{stability?.rcEffectiveLength} mm</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Rotação do RC:</Label>
-                    <Data>{stability?.rcRotation} mm</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Rotação do RA:</Label>
-                    <Data>{stability?.raRotation} mm</Data>
-                  </DataRow>
-                  <DataRow>
-                    <Label>Inclinação RW:</Label>
-                    <Data>{stability?.rwInclination}</Data>
-                  </DataRow>
-                </DataContainer>
-              </DataColumn>
-            </Columns>
-            <ButtonRow>
-              <button type="button">Baixar relatório</button>
-              <button
-                type="button"
-                onClick={() => openModalDeleteStabilityAnalysis()}
-              >
-                Excluir
-              </button>
-            </ButtonRow>
-            <ModalStyle
-              open={modalDeleteStabilityAnalysis}
-              onCancel={closeModalDeleteStabilityAnalysis}
-              footer={null}
-              width={500}
-              closeIcon={modalCloseButton}
-              destroyOnClose
-              centered
-              bodyStyle={{
-                background: '#123645',
-                color: 'white',
-              }}
-            >
-              <ModalDeleteStabilityAnalysis
-                close={closeModalDeleteStabilityAnalysis}
-              />
-            </ModalStyle>
-          </Container>
-        </>
-      ))}
+            Excluir
+          </button>
+        </ButtonRow>
+        <ModalStyle
+          open={modalDeleteStabilityAnalysis}
+          onCancel={closeModalDeleteStabilityAnalysis}
+          footer={null}
+          width={500}
+          closeIcon={modalCloseButton}
+          destroyOnClose
+          centered
+          bodyStyle={{
+            background: '#123645',
+            color: 'white',
+          }}
+        >
+          <ModalDeleteStabilityAnalysis
+            close={closeModalDeleteStabilityAnalysis}
+          />
+        </ModalStyle>
+      </Container>
     </div>
   );
 }
-// }
 
 StabilityAnalysisReport.propTypes = {
   data: PropTypes.object.isRequired,
--- src/pages/ReportSection/ReportSection.jsx
+++ src/pages/ReportSection/ReportSection.jsx
@@ -14,40 +14,85 @@ import {
   Reports,
   TESTEContainer,
 } from './Styles';
+import { useGetStabilityAnalysis } from '../../hooks/query/stabilityAnalysis';
+import useAuthStore from '../../stores/auth';
 
 export default function ReportSection() {
+  async function handleOpened(openedName) {
+    setOpened(openedName === opened ? '' : openedName);
+  }
+  const user = useAuthStore((state) => state.auth?.user);
+  const { data: data } = useGetStabilityAnalysis({});
+  const userStabilityAnalysis = data?.filter(
+    (stability) => stability?.user === user?._id
+  );
+
   const [name, setName] = useState('');
   const [opened, setOpened] = useState('');
 
-  async function handleOpened(openedName) {
-    setOpened((prevOpened) => (prevOpened === openedName ? '' : openedName));
+  if (user?.isAdmin) {
+    return (
+      <TESTEContainer>
+        <Container>
+          <Title>Relatórios</Title>
+          <ReportsArea>
+            <ReportsHeader>
+              <ReportsTitle>Relatório</ReportsTitle>
+              <SearchDiv>
+                <HiSearch size={25} />
+                <Search
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder={name || 'Pesquisar por nome'}
+                />
+              </SearchDiv>
+            </ReportsHeader>
+            <Reports>
+              {data?.map((report) => {
+                return (
+                  <StabilityAnalysisReport
+                    key={report}
+                    data={report}
+                    openedReport={opened}
+                    handleOpened={handleOpened}
+                  />
+                );
+              })}
+            </Reports>
+          </ReportsArea>
+        </Container>
+      </TESTEContainer>
+    );
+  } else {
+    return (
+      <TESTEContainer>
+        <Container>
+          <Title>Relatórios</Title>
+          <ReportsArea>
+            <ReportsHeader>
+              <ReportsTitle>Relatório</ReportsTitle>
+              <SearchDiv>
+                <HiSearch size={25} />
+                <SearchDiv
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder={name || 'Pesquisar por nome'}
+                />
+              </SearchDiv>
+            </ReportsHeader>
+            <Reports>
+              {userStabilityAnalysis?.map((report) => {
+                return (
+                  <StabilityAnalysisReport
+                    key={report}
+                    data={report}
+                    openedReport={opened}
+                    handleOpened={handleOpened}
+                  />
+                );
+              })}
+            </Reports>
+          </ReportsArea>
+        </Container>
+      </TESTEContainer>
+    );
   }
-
-  return (
-    <TESTEContainer>
-      <Container>
-        <Title>Relatórios</Title>
-        <ReportsArea>
-          <ReportsHeader>
-            <ReportsTitle>Relatório</ReportsTitle>
-            <SearchDiv>
-              <HiSearch size={25} />
-              <Search
-                onChange={(e) => setName(e.target.value)}
-                placeholder={name || 'Pesquisar por nome'}
-              />
-            </SearchDiv>
-          </ReportsHeader>
-          <Reports>
-            <StabilityAnalysisReport
-              key={name}
-              data={name}
-              openedReport={opened}
-              handleOpened={handleOpened}
-            />
-          </Reports>
-        </ReportsArea>
-      </Container>
-    </TESTEContainer>
-  );
 }
