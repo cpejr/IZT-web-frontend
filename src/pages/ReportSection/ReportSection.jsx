25fff5069793a2883f7b21d3d6b4f07f5a1638c2
Amanda Fernandes <amandaalves@cpejr.com.br>
2023-07-24 22:24:10 -0300

    fix: eslint errors

--- src/pages/ReportSection/ReportSection.jsx
+++ src/pages/ReportSection/ReportSection.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import { HiSearch } from 'react-icons/hi';
 
 import StabilityAnalysisReport from '../../components/features/Report/StabilityAnalysisReport';
+import { useGetStabilityAnalysis } from '../../hooks/query/stabilityAnalysis';
+import useAuthStore from '../../stores/auth';
 import {
   Container,
   Title,
@@ -14,21 +16,20 @@ import {
   Reports,
   TESTEContainer,
 } from './Styles';
-import { useGetStabilityAnalysis } from '../../hooks/query/stabilityAnalysis';
-import useAuthStore from '../../stores/auth';
 
 export default function ReportSection() {
+  const [name, setName] = useState('');
+  const [opened, setOpened] = useState('');
+
   async function handleOpened(openedName) {
     setOpened(openedName === opened ? '' : openedName);
   }
   const user = useAuthStore((state) => state.auth?.user);
-  const { data: data } = useGetStabilityAnalysis({});
+  const { data } = useGetStabilityAnalysis({});
+
   const userStabilityAnalysis = data?.filter(
     (stability) => stability?.user === user?._id
   );
-
-  const [name, setName] = useState('');
-  const [opened, setOpened] = useState('');
 
   if (user?.isAdmin) {
     return (
@@ -62,37 +63,36 @@ export default function ReportSection() {
         </Container>
       </TESTEContainer>
     );
-  } else {
-    return (
-      <TESTEContainer>
-        <Container>
-          <Title>Relatórios</Title>
-          <ReportsArea>
-            <ReportsHeader>
-              <ReportsTitle>Relatório</ReportsTitle>
-              <SearchDiv>
-                <HiSearch size={25} />
-                <SearchDiv
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder={name || 'Pesquisar por nome'}
-                />
-              </SearchDiv>
-            </ReportsHeader>
-            <Reports>
-              {userStabilityAnalysis?.map((report) => {
-                return (
-                  <StabilityAnalysisReport
-                    key={report}
-                    data={report}
-                    openedReport={opened}
-                    handleOpened={handleOpened}
-                  />
-                );
-              })}
-            </Reports>
-          </ReportsArea>
-        </Container>
-      </TESTEContainer>
-    );
   }
+  return (
+    <TESTEContainer>
+      <Container>
+        <Title>Relatórios</Title>
+        <ReportsArea>
+          <ReportsHeader>
+            <ReportsTitle>Relatório</ReportsTitle>
+            <SearchDiv>
+              <HiSearch size={25} />
+              <SearchDiv
+                onChange={(e) => setName(e.target.value)}
+                placeholder={name || 'Pesquisar por nome'}
+              />
+            </SearchDiv>
+          </ReportsHeader>
+          <Reports>
+            {userStabilityAnalysis?.map((report) => {
+              return (
+                <StabilityAnalysisReport
+                  key={report}
+                  data={report}
+                  openedReport={opened}
+                  handleOpened={handleOpened}
+                />
+              );
+            })}
+          </Reports>
+        </ReportsArea>
+      </Container>
+    </TESTEContainer>
+  );
 }
