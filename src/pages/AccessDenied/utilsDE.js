54fe1489f386ca091d60c9062d37f4b0535737b2
Rian-Rero <rian@dharma.com.br>
2023-10-14 11:51:59 -0300

    feat: accessDenied translated

---
+++ src/pages/AccessDenied/utilsDE.js
@@ -0,0 +1,15 @@
+/* eslint-disable import/prefer-default-export */
+export const contentOptionsDE = {
+  course: {
+    title: 'Sie haben noch keinen Zugriff auf unseren Kurs!',
+    text: 'Es tut uns leid, Ihnen mitteilen zu müssen, dass Ihnen der Zugriff auf den Kurs verweigert wurde. Dieser Kurs ist ausschließlich für diejenigen reserviert, die sich im Voraus angemeldet haben. Wir stehen jedoch zur Verfügung, um Ihnen beim Erwerb dieses Kurses und anderer Produkte, die für Sie interessant sein könnten, zu helfen. Bitte kontaktieren Sie uns über das unten stehende Formular, um ihn zu erwerben.',
+    formTitle: 'Holen Sie sich unseren Kurs!',
+    imageURL: 'src/assets/accessDeniedPage/BGCourse.png',
+  },
+  software: {
+    title: 'Sie haben noch keinen Zugriff auf unsere Software!',
+    text: 'Es tut uns leid, Ihnen mitteilen zu müssen, dass Ihnen der Zugriff auf unsere Software verweigert wurde. Diese Software ist ausschließlich für diejenigen reserviert, die sich im Voraus angemeldet haben. Wir stehen jedoch zur Verfügung, um Ihnen beim Erwerb dieser Software und anderer Produkte, die für Sie interessant sein könnten, zu helfen. Bitte kontaktieren Sie uns über das unten stehende Formular, um Zugang zu erhalten.',
+    formTitle: 'Erhalten Sie Zugang!',
+    imageURL: 'src/assets/accessDeniedPage/BGSoftware.jpg',
+  },
+};
